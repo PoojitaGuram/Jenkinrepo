743da1e4b3a0b545f3eea2260130ba99b132c19f
PoojitaGuram <poojitaguram16@gmail.com>
2023-11-29 17:04:03 +0530

    fg

---
+++ src/test/java/com/poo123/Jenkin2test.java
@@ -0,0 +1,14 @@
+package com.poo123;
+
+import org.testng.annotations.Test;
+
+public class Jenkin2test {
+	@Test(groups = "smoke")
+	public void jtest() {
+		System.out.println("hi hello");
+	}
+	@Test(groups = "regression")
+	public void htest() {
+		System.out.println("poo");
+	}
+}
--- src/test/java/com/poo123/JenkinTest.java
+++ src/test/java/com/poo123/JenkinTest.java
@@ -3,8 +3,12 @@ package com.poo123;
 import org.testng.annotations.Test;
 
 public class JenkinTest {
-@Test
+@Test(groups = "smoke")
 public void demotest() {
 	System.out.println("hi hello");
+}
+@Test(groups = "regression")
+public void demo1test() {
+	System.out.println("poo");
 }
 }
